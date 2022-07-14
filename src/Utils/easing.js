d30b437a54b8eacce2c15fa4ddff46c58db6e698
Jeet <jeetpatel0070070@gmail.com>
2022-07-14 17:50:59 +0530

    basic setup

---
+++ src/Utils/easing.js
@@ -0,0 +1,9 @@
+export const cssEasing = {
+  easeInOut: "cubic-bezier(0.445, 0.05, 0.55, 0.95)",
+  easeOut: "cubic-bezier(0.23, 1, 0.32, 1)",
+  easeIn: "cubic-bezier(0.42, 0, 1, 1)",
+  ease: "cubic-bezier(0.25, 0.1, 0.25, 1)",
+  sharp: "cubic-bezier(0.4, 0, 0.6, 1)",
+  linear: "cubic-bezier(0, 0, 1, 1)",
+  default: "cubic-bezier(0.77, 0, 0.175, 1)"
+};
