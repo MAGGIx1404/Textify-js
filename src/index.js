aad36cc13780bff5fcdf93d55550599ccb210680
Jeet <74851359+MAGGIx1404@users.noreply.github.com>
2022-07-18 12:28:49 +0530

    Test (#15)
    * add remove examples
    
    * remove old examples
    
    * add custom eases
    
    * add custom eases
    
    * add custom eases

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import Text from "./Animations/Text";
+import { mapEach } from "./Utils/dom";
+
 export default class Textify {
   constructor() {
+    this.element = document.querySelectorAll("[data-textify]");
+    console.log(this.element);
+    this.createAnimation();
+    this.events();
   }
 
   createAnimation() {
@@ -13,5 +20,8 @@ export default class Textify {
   }
 
   onResize() {
+    this.animation.forEach((animation) => {
+      animation.onResize();
+    });
   }
 }
