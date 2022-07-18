73064061721459681aae3f6c9bf6f02eddea21b8
Jeet <jeetpatel0070070@gmail.com>
2022-07-18 11:12:09 +0530

    remove old examples

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { each } from "lodash";
 import { cssEasing } from "./Utils/easing";
-import { calculate, split } from "./Utils/texts";
+// import { calculate, split } from "./Utils/texts";
 // import browserCheck from "./Utils/browserCheck";
 import Animation from "./Components/Animation";
 
@@ -12,8 +12,8 @@ export default class Textify extends Animation {
     // const paragraphs = element.querySelectorAll("h1, h2, p");
 
     element.forEach((el) => {
-      split({ el });
-      split({ el });
+      this.split({ el });
+      this.split({ el });
       lines.push(...element.querySelectorAll("span span"));
     });
 
@@ -55,7 +55,141 @@ export default class Textify extends Animation {
   }
 
   onResize() {
-    this.lines = calculate(this.elements.lines);
+    this.lines = this.calculate(this.elements.lines);
+  }
+
+  split({ expression = " ", append = true }) {
+    const element = [...document.querySelectorAll("[data-textify-animation]")];
+    let words;
+    for (let i = 0; i <= element.length; i++) {
+      words = this.splitText(element[i].innerHTML, expression);
+      element[i].innerHTML = "";
+      words.forEach((word) => {
+        element[i].innerHTML += this.parseLine(word);
+      });
+    }
+
+    let innerHTML = "";
+
+    each(words, (line) => {
+      if (line.indexOf("<br>") > -1) {
+        const lines = line.split("<br>");
+
+        each(lines, (line, index) => {
+          innerHTML += index > 0 ? "<br>" + this.parseLine(line) : this.parseLine(line);
+        });
+      } else {
+        innerHTML += this.parseLine(line);
+      }
+    });
+
+    element.innerHTML = innerHTML;
+
+    const spans = element.querySelectorAll("span");
+
+    if (append) {
+      each(spans, (span) => {
+        const isSingleLetter = span.textContent.length === 1;
+        const isNotEmpty = span.innerHTML.trim() !== "";
+        const isNotAndCharacter = span.textContent !== "&";
+        const isNotDashCharacter = span.textContent !== "-";
+
+        if (isSingleLetter && isNotEmpty && isNotAndCharacter && isNotDashCharacter) {
+          span.innerHTML = `${span.textContent}&nbsp;`;
+        }
+      });
+    }
+
+    return spans;
+  }
+
+  calculate(spans) {
+    const lines = [];
+    let words = [];
+
+    let position = spans[0].offsetTop;
+
+    each(spans, (span, index) => {
+      if (span.offsetTop === position) {
+        words.push(span);
+      }
+
+      if (span.offsetTop !== position) {
+        lines.push(words);
+
+        words = [];
+        words.push(span);
+
+        position = span.offsetTop;
+      }
+
+      if (index + 1 === spans.length) {
+        lines.push(words);
+      }
+    });
+
+    return lines;
+  }
+
+  splitText(text, expression) {
+    const splits = text.split("<br>");
+
+    let words = [];
+
+    each(splits, (item, index) => {
+      if (index > 0) {
+        words.push("<br>");
+      }
+
+      words = words.concat(item.split(expression));
+
+      let isLink = false;
+      let link = "";
+
+      const innerHTML = [];
+
+      each(words, (word) => {
+        if (!isLink && (word.includes("<a") || word.includes("<strong"))) {
+          link = "";
+
+          isLink = true;
+        }
+
+        if (isLink) {
+          link += ` ${word}`;
+        }
+
+        if (isLink && (word.includes("/a>") || word.includes("/strong>"))) {
+          innerHTML.push(link);
+
+          link = "";
+        }
+
+        if (!isLink && link === "") {
+          innerHTML.push(word);
+        }
+
+        if (isLink && (word.includes("/a>") || word.includes("/strong>"))) {
+          isLink = false;
+        }
+      });
+
+      words = innerHTML;
+    });
+
+    return words;
+  }
+
+  parseLine(line) {
+    if (line === "") {
+      return line;
+    } else if (line === " ") {
+      return "&nbsp;";
+    } else {
+      line = line.trim();
+
+      return line === "<br>" ? "<br>" : `<span>${line}</span>` + (line.length > 1 ? " " : "");
+    }
   }
 }
 
